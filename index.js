b2f0c7ae2f01e5ace11d655def351939c91fe146
Jenifer Fabian Dubon <jfabiandub@brynmawr.edu>
2023-03-26 13:44:45 -0400

    edit endpoint done

--- index.js
+++ index.js
@@ -38,11 +38,14 @@ The goal for this endpoint is to add new data (aka new questions ) to the Survey
 app.use('/add', (req, res) =>{
     const surveyId = req.body.surveyId;
         Survey.findOneAndUpdate(
-            { _id: surveyId },
-            { $push: { questions: req.body.questions } },
+            { _id: surveyId }, //id so we can get the survey we want to update
+            { $push: { questions: req.body.questions } }, //adds the new question into the array and updates
             { new: true }
         )
             .then(survey => {
+                if(!survey){
+                    res.status(200).json({error : "Survey not found"});
+                }
                 console.log('Survey updated:', survey);
                 res.json({ success: 'Survey updated successfully.' });
             })
@@ -54,13 +57,37 @@ app.use('/add', (req, res) =>{
 
 app.use('/edit', (req, res) => {
     //this one will allow to edit the questions in the survey from an administrator side.
+    const surveyId = req.body.surveyId;
+    Survey.findOneAndUpdate({_id: surveyId },
+        { $set: { questions: req.body.questions } },
+        { new: true }
+    )
+        .then(survey => {
+            if(!survey){
+                res.status(200).json({error : "Survey not found"});
+            }
+            console.log('Survey updated:', survey);
+            res.json({ success: 'Survey updated successfully.' });
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({ error: 'Survey could not be updated.' });
+        });
 
 });
+
+/*
+ok so findAndUpdate is good to add and edit stuff, the fields that should be changed are
+the update case, $push basically add an elemnet to the array (aka our questions)
+the $set updates an element in the array. aka edit one of the questions
+$pull removes and element
+ */
 
 
 /*I edited the index.html file so it shows me to create the survey. and the /add feature. I think we have to fix it though
  b/c rn, it shows me both tasks on the same page, and ideally i think we want the create survey, and then
  in the view feature should be a button to add a question to the survey (i think?
+ $inc is to increase or decrease a number field
  */
 
 app.use('/home', (req, res) => {
