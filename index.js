1010528bd34e79e9323074cb1eef12302d110967
Jenifer Fabian Dubon <jfabiandub@brynmawr.edu>
2023-03-26 23:37:34 -0400

    update edit

--- index.js
+++ index.js
@@ -14,47 +14,81 @@ const Question = require('./Question.js');
 //This endpoint creates the Survey with a title, a description, and a set of qeustions
 app.use('/create', (req, res)=> {
 	// construct the Question from the form data which is in the request body
-	var newQuestion = new Question ({
+    var newQuestion = new Question ({
 		questionText: req.body.questionText, 
         });
-
-    newQuestion.save( (err) => { 
-        if (err) {
-            res.type('html').status(200);
-            res.write('uh oh: ' + err);
-            console.log(err);
-            res.end();
-        }
-        else {
-        // display the "successfull created" message
-            res.send('successfully added ' + newQuestion.questionText + ' to the database');
-        }
+    newQuestion.save()
+        .then(() => {
+            console.log('Survey added:', newQuestion);
+            res.redirect('/all.html');
+           // res.json({ success: 'Question' + ' " ' + newQuestion.questionText + ' " ' + ' saved successfully.' });
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).json({ error: 'Survey could not be saved.' });
         });
+});
 
+
+//so i used app.get bc those are the ones that deal with the http requests.
+
+/* This endpoint uses app.get to edit a question. The app.get renders the edit form  */
+app.get('/edit', (req, res) => {
+    const questionText = req.query.text;
+
+    // Find the question in the database
+    Question.findOne({questionText}, (err, question) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({error: 'Could not find question.'});
+        } else {
+            // Render the HTML form for editing the question
+            res.send(`
+        <html>
+          <head>
+           <link rel="stylesheet" href="app.css" />
+            <link
+            rel="stylesheet"
+            href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css"
+            />
+            <title>Please Edit Question</title>
+          </head>
+          <body>
+            <h1>Edit Question</h1>
+            <form method="POST" action="/edit">
+              <label for="questionText">Question Text:</label>
+              <input type="text" name="questionText" value="${question.questionText}">
+              <input type="hidden" name="questionId" value="${question._id}">
+              <input type="submit" value="Update">
+            </form>
+          </body>
+        </html>
+      `);
+        }
     });
+});
 
 
-//endpoint to add survey data
-//The goal for this endpoint is to add new data (aka new questions ) to the Survey and it updates it.
-//Leaving for Jenii to decide
-app.use('/add', (req, res) =>{
-    const surveyId = req.body.surveyId;
-        Survey.findOneAndUpdate(
-            { _id: surveyId }, //id so we can get the survey we want to update
-            { $push: { questions: req.body.questions } }, //adds the new question into the array and updates
-            { new: true }
-        )
-            .then(survey => {
-                if(!survey){
-                    res.status(200).json({error : "Survey not found"});
-                }
-                console.log('Survey updated:', survey);
-                res.json({ success: 'Survey updated successfully.' });
-            })
-            .catch(error => {
-                console.error(error);
-                res.status(500).json({ error: 'Survey could not be updated.' });
-            });
+/*  This endpoint uses app.post because it handles HTTP POST request specifically */
+
+app.post('/edit', (req, res) => {
+    const { questionText, questionId } = req.body;
+
+    // Update the question in the database
+    Question.findByIdAndUpdate(
+        questionId,
+        { questionText },
+        { new: true },
+        (err, question) => {
+            if (err) {
+                console.error(err);
+                res.status(500).json({ error: 'Could not update question.' });
+            } else {
+                console.log('Question updated:', question);
+                res.redirect('/all.html');
+            }
+        }
+    );
 });
 
 
@@ -82,7 +116,8 @@ app.use('/all', (req, res) => {
 
 app.use('/delete', (req, res) => {
 
-    res.json({ success: 'Deleted' });
+    //res.json({ success: 'Deleted' });
+    res.redirect('/all.html');
 
 });
 
